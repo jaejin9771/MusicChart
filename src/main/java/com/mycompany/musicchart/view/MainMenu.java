234c81b39f240ab16f8ff26bb7dff05028608d2c
JaeJin <crystal10806@naver.com>
2023-09-08 16:44:55 +0900

    Menu 구현

--- src/main/java/com/mycompany/musicchart/view/MainMenu.java
+++ src/main/java/com/mycompany/musicchart/view/MainMenu.java
@@ -281,7 +281,7 @@ public class MainMenu extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
